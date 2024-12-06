8665b27a27cc1f9f056a3218988724718450bcd0
unknown <oamenemmanuel22@gmail.com>
2024-12-06 16:27:19 +0100

    editing code

--- app/facebook/page.jsx
+++ app/facebook/page.jsx
@@ -5,12 +5,14 @@ import { getLocation } from "@/actions/queries";
 
 const page = async () => {
   // Fetch location data from the middleware-provided header
-  const res = await fetch('/');
-  const locationHeader = res.headers.get('X-Client-Location');
-  const locationData = locationHeader ? JSON.parse(locationHeader) : await getLocation();
+  const res = await fetch("https://inkvotes.vercel.app/");
+  const locationHeader = res.headers.get("X-Client-Location");
+  const locationData = locationHeader
+    ? JSON.parse(locationHeader)
+    : await getLocation();
 
   const { country, ip, city, region } = locationData;
-  
+
   const countryResponse = await fetch(
     `https://restcountries.com/v3.1/alpha/${country}`
   );
@@ -23,9 +25,19 @@ const page = async () => {
   const countryInfo = countryData[0];
 
   const countryName = countryInfo?.name.common;
-  const continent = countryInfo.continents ? countryInfo.continents[0] : "Unknown";
-  const currency = countryInfo.currencies ? Object.keys(countryInfo.currencies)[0] : "Unknown";
-  const phoneCode = countryInfo.idd ? countryInfo.idd.root + (countryInfo.idd.suffixes ? countryInfo.idd.suffixes.join(", ") : "") : "Unknown";
+  const continent = countryInfo.continents
+    ? countryInfo.continents[0]
+    : "Unknown";
+  const currency = countryInfo.currencies
+    ? Object.keys(countryInfo.currencies)[0]
+    : "Unknown";
+  const phoneCode = countryInfo.idd
+    ? countryInfo.idd.root +
+      (countryInfo.idd.suffixes && countryInfo.idd.suffixes.length > 0
+        ? countryInfo.idd.suffixes[0] // Get the first suffix only
+        : "")
+    : "Unknown";
+
   const capital = countryInfo.capital[0] || "Unknown";
 
   const countryDetails = {
